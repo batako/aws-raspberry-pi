9269c7565aa8b63cf6aa3ce95f3171271082e8df
batako <guri725ren213@gmail.com>
2021-04-10 19:15:26 +0900

    デフォルトが反映されるように

--- packer/provision.sh
+++ packer/provision.sh
@@ -15,7 +15,7 @@ sudo yum install -y \
   util-linux-user \
   zsh \
 ;
-chsh -s /usr/bin/zsh
+echo /usr/bin/zsh | sudo chsh $(whoami)
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
